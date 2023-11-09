41cb1c05815869c8a60f2fb8c046b126309e6e97
Kim <jdhnt01@gmail.com>
2023-11-09 17:42:02 +0900

    1

--- scan.py
+++ scan.py
@@ -15,15 +15,16 @@ ip = input("IP : ")
 while True:
     if num == "1": # TCP_SYN 스캔
         tcp_ports = synscan.scan(ip)
+        print(tcp_ports)
         tcp_services = servicescan.ServiceScan(ip,tcp_ports,'tcp')
-        print(tcp_services)
+        
         for tcp_service in tcp_services:
             print(f"PORT : {tcp_service[0]} SERVICE : {tcp_service[1]}")
         break
     if num == "2" : # UDP 스캔
         udp_ports = udpscan.udp_scan(ip)
+        print(udp_ports)
         udp_services = servicescan.ServiceScan(ip,udp_ports,'udp')
-        print(udp_services)
         for udp_service in udp_services:
             print(f"PORT : {udp_service[0]} SERVICE : {udp_service[1]}")
         break
