63b4c594d729d749ce78486d7025184df2fb1d69
Kim <jdhnt01@gmail.com>
2023-11-09 17:40:57 +0900

    1

--- test/test.py
+++ test/test.py
@@ -2,17 +2,17 @@ import socket
 import ftplib
 import datetime
 
-ip = "192.168.229.131"
-port = 45557
+ip = "218.50.136.184"
+port = 9094
 
 def check_DNS(ip, port):
     print(f"{port} : checking DNS...")
-    message = b'\xaa\xaa\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x07version\x04bind\x00\x00\x10\x00\x03'
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((ip,port))
     sock.settimeout(3)
     try:
-        sock.sendto(message,(ip, port))
-        data, _ = sock.recvfrom(512)
+        data = sock.recv(1024)
+        print(data)
         return True
     except Exception as e:
         return False
