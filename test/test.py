1d877720924b002f4ce9208397855ee012306b2d
Kim <jdhnt01@gmail.com>
2023-11-09 17:26:57 +0900

    1

--- test/test.py
+++ test/test.py
@@ -3,27 +3,22 @@ import ftplib
 import datetime
 
 ip = "192.168.229.131"
-port = 123
+port = 45557
 
-def check_NTP(ip,port):
-    print(f"{port} : checking NTP...")
-    s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    # NTP PROBE 생성
-    pack = b"\xe3\x00\x04\xfa\x00\x01\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00"\
-    b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"\
-    b"\x00\x00\x00\x00\x00\x00\x00\x00\xc5\x4f\x23\x4b\x71\xb1\x52\xf3"
-
-    s.settimeout(5)
+def check_DNS(ip, port):
+    print(f"{port} : checking DNS...")
+    message = b'\xaa\xaa\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x07version\x04bind\x00\x00\x10\x00\x03'
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(3)
     try:
-        s.sendto(pack,(ip,port))
-        recv, server = s.recvfrom(1024)
-        if recv.__len__() >= 48 and (0 < int(recv[1] < 15)): # 패킷 최소 길이 및 startum(0~15) 값인지 확인
-            return True
-        return False
-    except:
+        sock.sendto(message,(ip, port))
+        data, _ = sock.recvfrom(512)
+        return True
+    except Exception as e:
         return False
     finally:
-        s.close()
+        sock.close()
 
-if(check_NTP(ip,port)):
+
+if(check_DNS(ip,port)):
     print('mysql')
